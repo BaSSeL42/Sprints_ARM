474aadc7a3bca44ba7418b2c3d26a56eeabd9e79
BaSSeL YaSSeR <104660108+BaSSeL42@users.noreply.github.com>
2023-06-18 18:23:37 +0300

    Video & Documentation

--- led_v1/Code/APP/app.c
+++ led_v1/Code/APP/app.c
@@ -8,9 +8,9 @@
 #define RED							MGPIO_PINF_1
 #define BLUE						MGPIO_PINF_2
 #define GREEN						MGPIO_PINF_3
-#define PUSH_BTN				MGPIO_PINF_0
+#define PUSH_BTN				MGPIO_PINF_4
 
-static void delay_ms(uint32_ number_of_seconds);
+
 static uint8_ u8_gs_btn_state = MGPIO_PIN_HIGH;
 static uint8_ u8_gs_btn_prv_state = MGPIO_PIN_HIGH;
 static uint8_ u8_gs_btn_press_counter = 4;
@@ -35,6 +35,7 @@ void APP_vidInit(void)
 void APP_vidStart(void)
 {
 	HButton_getPinVal(PUSH_BTN,&u8_gs_btn_state);
+	
 	if(MGPIO_PIN_LOW == u8_gs_btn_state && MGPIO_PIN_HIGH == u8_gs_btn_prv_state)
 	{
 		if(4 == u8_gs_btn_press_counter)
@@ -46,68 +47,50 @@ void APP_vidStart(void)
 			u8_gs_btn_press_counter++;
 		}
 	}
+
 	switch(u8_gs_btn_press_counter)
 	{
-		case 0 :
-		{
-			HLed_on(RED);			// RED On
-			HLed_off(BLUE);			// BLUE	OFF
-			HLed_off(GREEN);		// GREEN OFF
-			break;
-		}
-		case 1 :
-		{
-			HLed_off(RED);			// RED OFF
-			HLed_off(BLUE);			// BLUE	OFF
-			HLed_on(GREEN);		// GREEN On
-			break;
-		}
-		case 2 :
-		{
-			HLed_off(RED);			// RED OFF
-			HLed_on(BLUE);			// BLUE	On
-			HLed_off(GREEN);		// GREEN OFF
-			break;
-		}
-		case 3 :
-		{
-			HLed_on(RED);			// RED On
-			HLed_on(BLUE);		// BLUE	On
-			HLed_on(GREEN);		// GREEN On
-			break;
-		}
-		case 4 :
-		{
-			HLed_off(RED);			// RED OFF
-			HLed_off(BLUE);			// BLUE	OFF
-			HLed_off(GREEN);		// GREEN OFF
-			break;
-		}
-		default :
-		{
-			break;
-		}
-	}
-	//HLed_on(MGPIO_PINF_1);			// RED On
-	
+							case 0 :
+							{
+								HLed_on(RED);			// RED On
+								HLed_off(BLUE);			// BLUE	OFF
+								HLed_off(GREEN);		// GREEN OFF
+								break;
+							}
+							case 1 :
+							{
+								HLed_off(RED);			// RED OFF
+								HLed_off(BLUE);			// BLUE	OFF
+								HLed_on(GREEN);		// GREEN On
+								break;
+							}
+							case 2 :
+							{
+								HLed_off(RED);			// RED OFF
+								HLed_on(BLUE);			// BLUE	On
+								HLed_off(GREEN);		// GREEN OFF
+								break;
+							}
+							case 3 :
+							{
+								HLed_on(RED);			// RED On
+								HLed_on(BLUE);		// BLUE	On
+								HLed_on(GREEN);		// GREEN On
+								break;
+							}
+							case 4 :
+							{
+								HLed_off(RED);			// RED OFF
+								HLed_off(BLUE);			// BLUE	OFF
+								HLed_off(GREEN);		// GREEN OFF
+								break;
+							}
+							default :
+							{
+								break;
+							}
+	}	
 	u8_gs_btn_prv_state = u8_gs_btn_state;
-}
-
-
-
-static void delay_ms(uint32_ number_of_seconds)
-{
-    // Converting time into milli_seconds
-    uint32_ i,j;
-	
-	for (i = 0; i < number_of_seconds; i++)
-	{
-		for (j = 0; j < 1275; j++)
-		{
-			__asm__("NOP");
-			
-		}
-	}
 }
 
 
