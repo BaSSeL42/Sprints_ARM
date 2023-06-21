609660e9c65c44652cbf19f759d4fa47d3635473
Mahmoud Matarawy <30804086+MahmoudMatarawy@users.noreply.github.com>
2023-06-21 04:05:09 +0300

    update LED_V3

---
+++ led_v3/Objects/gpt_manager.d
@@ -0,0 +1,4 @@
+.\objects\gpt_manager.o: code\HAL\gpt_manager\gpt_manager.c
+.\objects\gpt_manager.o: .\code\HAL\gpt_manager\include\gpt_manager.h
+.\objects\gpt_manager.o: .\code\MCAL\gpt\include\gpt_Interface.h
+.\objects\gpt_manager.o: .\code\COM\LSTD_TYPES.h
--- led_v3/code/APP/app.c
+++ led_v3/code/APP/app.c
@@ -4,46 +4,68 @@
 #include "led.h"
 #include "button.h"
 #include "app.h"
-#include "systick_manager.h"
+#include "gpt_manager.h"
 //#include "systick_Interface.h"
 
-#define RED							MGPIO_PINF_1
-#define BLUE						MGPIO_PINF_2
+//#define RED							MGPIO_PINF_1
+//#define BLUE						MGPIO_PINF_2
 #define GREEN						MGPIO_PINF_3
 #define PUSH_BTN				MGPIO_PINF_4
 
-void v_systick_cbf(void);
+//void v_systick_cbf(void);
 static void APP_vidCBK(void);
 static uint8_ u8_gs_btn_state = MGPIO_PIN_HIGH;
 static uint8_ u8_gs_btn_prv_state = MGPIO_PIN_HIGH;
-static uint8_ u8_gs_btn_press_counter = 4;
-static uint8_ u8_gs_prv_btn_press_counter = 4;
+const uint8_ u8_gc_arr_duty_cycles[5] = {0,30,60,90};
+static uint8_ u8_gs_btn_press_counter = 0;
+static uint8_ u8_gs_current_duty_cycle = 0;
+static uint8_ u8_gs_duty_counter = 0;
+uint64_ p_u64_int_cur_val;
+
 
 void APP_vidInit(void)
 {
-	HLed_Init(RED);				// RED
-	HLed_Init(BLUE);			// BLUE
 	HLed_Init(GREEN);			// GREEN
 	
 	
 	
 	HButton_Init(PUSH_BTN);			// Push button
 	
-	st_systk_cfg_t st_systk_cfg = {
-		.en_systck_clk_src = SYSTK_SYSTEM_CLK,
-		.en_systck_int = SYSTK_IRQ_ENABLE,
-		.u32_time_ms = 1000,
-		.ptr_func = v_systick_cbf
 	
+	st_gpt_timer_cfg_t st_gpt_timer_cfg = {
+	
+		.en_gpt_ch_id = GPT_CHANNEL_0,
+		.en_gpt_mode = GPT_CH_MODE_PERIODIC,
+		.en_gpt_stall = GPT_STALL_DISABLE,
+		.en_gpt_time_x = GPT_TIME_MS,
+		.u32_set_time = 50,
+		.en_gpt_irq = GPT_IRQ_ENABLE,
+		.ptr_func = APP_vidCBK
 	};
-	SYSTICK_MANAGER_u8Init(&st_systk_cfg);
 	
+	GPT_MANAGER_u8Init(&st_gpt_timer_cfg);
 	
-	HLed_off(RED);			// RED OFF
-	HLed_off(BLUE);			// BLUE	OFF
+	//GPT_MANAGER_u8DutyCycle(40);
+	GPT_MANAGER_u8Start();
+	
 	HLed_off(GREEN);		// GREEN OFF
 }
 
+void duty(uint8_ duty )
+{
+	if(((duty/10) > u8_gs_duty_counter) && (0<= u8_gs_duty_counter))
+	{
+		HLed_on(GREEN);
+	}
+	else if (((duty/10) <= u8_gs_duty_counter) && (10> u8_gs_duty_counter))
+	{
+		HLed_off(GREEN);
+	}
+	else
+	{
+		u8_gs_duty_counter = 0;
+	}
+}
 
 void APP_vidStart(void)
 {
@@ -51,57 +73,19 @@ void APP_vidStart(void)
 	
 	if(MGPIO_PIN_LOW == u8_gs_btn_state && MGPIO_PIN_HIGH == u8_gs_btn_prv_state)
 	{
-		if(4 == u8_gs_prv_btn_press_counter)
+		if(3 == u8_gs_btn_press_counter)
 		{
-			u8_gs_prv_btn_press_counter = 0;
 			u8_gs_btn_press_counter = 0;
+			u8_gs_current_duty_cycle = u8_gc_arr_duty_cycles[0];
 		}
-		else if(4 >  u8_gs_prv_btn_press_counter)
+		else if(3 >  u8_gs_btn_press_counter)
 		{
-			u8_gs_prv_btn_press_counter++;
-			u8_gs_btn_press_counter = u8_gs_prv_btn_press_counter;
+			u8_gs_btn_press_counter++;
+			u8_gs_current_duty_cycle = u8_gc_arr_duty_cycles[u8_gs_btn_press_counter];
 		}
 	}
+	duty(u8_gs_current_duty_cycle);
 
-	if(0 == u8_gs_btn_press_counter)
-	{
-		SYSTICK_MANAGER_vidStart();	// start timer
-		HLed_on(RED);			// RED On
-		HLed_off(BLUE);			// BLUE	OFF
-		HLed_off(GREEN);		// GREEN OFF
-	}
-	else if (1 == u8_gs_btn_press_counter)
-	{
-		SYSTICK_MANAGER_vidStart();	// start timer
-		HLed_off(RED);			// RED OFF
-		HLed_off(BLUE);			// BLUE	OFF
-		HLed_on(GREEN);		// GREEN On
-	}
-	else if (2 == u8_gs_btn_press_counter)
-	{
-		SYSTICK_MANAGER_vidStart();	// start timer
-		HLed_off(RED);			// RED OFF
-		HLed_on(BLUE);			// BLUE	On
-		HLed_off(GREEN);		// GREEN OFF
-	}
-	else if(3 == u8_gs_btn_press_counter)
-	{
-		SYSTICK_MANAGER_vidStart();	// start timer
-		HLed_on(RED);			// RED On
-		HLed_on(BLUE);		// BLUE	On
-		HLed_on(GREEN);		// GREEN On
-	}
-	else if(4 == u8_gs_btn_press_counter)
-	{
-		SYSTICK_MANAGER_vidStop();		// stop timer
-		HLed_off(RED);			// RED OFF
-		HLed_off(BLUE);			// BLUE	OFF
-		HLed_off(GREEN);		// GREEN OFF
-	}
-	else
-	{
-		// do nothing
-	}
 	
 	u8_gs_btn_prv_state = u8_gs_btn_state;
 }
@@ -111,29 +95,24 @@ void APP_vidStart(void)
 
 static void APP_vidCBK(void)
 {
-	static boolean flag = FALSE;
+	u8_gs_duty_counter++;
+	/*static boolean flag = FALSE;
 
 	if(flag == FALSE)
 	{
-		(void) HLed_off(MGPIO_PINF_3);
+		(void) HLed_off(GREEN);
 		GPT_u8DutyCycle(gs_u32_duty_cycle_val);
 		flag = TRUE;
 	}
 	else if (flag == TRUE)
 	{
-		(void) HLed_on(MGPIO_PINF_3);
+		(void) HLed_on(GREEN);
 		GPT_u8DutyCycle(gs_u32_duty_cycle_flag);
 		flag = FALSE;
-	}
+	}*/
 }
 
 
-
-void v_systick_cbf(void)
-{
-	//u8_gs_btn_press_counter++;
-	u8_gs_btn_press_counter = 4;
-}
 
 
 
