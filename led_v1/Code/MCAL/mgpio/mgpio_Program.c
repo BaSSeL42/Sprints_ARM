0f15217120958d28d88f143434615b0c73abc4cd
BaSSeL YaSSeR <104660108+BaSSeL42@users.noreply.github.com>
2023-06-22 23:55:40 +0300

    MGPIO Modified

--- led_v1/Code/MCAL/mgpio/mgpio_Program.c
+++ led_v1/Code/MCAL/mgpio/mgpio_Program.c
@@ -51,43 +51,75 @@ uint8_ MGPIO_u8Init(st_gpio_cfg_t* st_gpio_cfg)
 			
 			SET_BIT(RCGCGPIO_REG, loc_u8_PortNum );
 			
-			switch (st_gpio_cfg->enu_gpio_mode)
+			
+			if (st_gpio_cfg->enu_gpio_mode == MGPIO_MODE_DIGITAL)
 			{
-				case MGPIO_MODE_DIGITAL:
 							switch (st_gpio_cfg->enu_pin_dir_mode)
 							{
 								case MGPIO_DIR_INPUT	:	CLR_BIT(GPIODIR(loc_u8_PortNum), loc_u8_PinNum );
-																				switch (st_gpio_cfg->un_gpio_conf.u8_input_pull_type)
+																				if (st_gpio_cfg->un_gpio_conf.u8_input_pull_type == MGPIO_NO_PULL)
 																				{
-																					case MGPIO_NO_PULL		: CLR_BIT(GPIOPUR(loc_u8_PortNum), loc_u8_PinNum ); CLR_BIT(GPIOPDR(loc_u8_PortNum), loc_u8_PinNum );		break;
-																					case MGPIO_PULL_UP		:	SET_BIT(GPIOPUR(loc_u8_PortNum), loc_u8_PinNum );		break;
-																					case MGPIO_PULL_DOWN	:	SET_BIT(GPIOPDR(loc_u8_PortNum), loc_u8_PinNum );		break;
-																					default:  break;
+																					CLR_BIT(GPIOPUR(loc_u8_PortNum), loc_u8_PinNum ); 
+																					CLR_BIT(GPIOPDR(loc_u8_PortNum), loc_u8_PinNum );	
+																				}
+																				else if(st_gpio_cfg->un_gpio_conf.u8_input_pull_type == MGPIO_PULL_UP)
+																				{
+																					SET_BIT(GPIOPUR(loc_u8_PortNum), loc_u8_PinNum );	
+																				}
+																				else if(st_gpio_cfg->un_gpio_conf.u8_input_pull_type == MGPIO_PULL_DOWN)
+																				{
+																					SET_BIT(GPIOPDR(loc_u8_PortNum), loc_u8_PinNum );	
+																				}
+																				else
+																				{
+																					/* do nothing */
 																				}
 																				break;
 
 								case MGPIO_DIR_OUTPUT	:	SET_BIT(GPIODIR(loc_u8_PortNum), loc_u8_PinNum );
-																				switch (st_gpio_cfg->un_gpio_conf.enu_gpio_amp_mode )
+																				
+																				if (st_gpio_cfg->un_gpio_conf.enu_gpio_amp_mode == MGPIO_OPEN_DRAIN)
 																				{
-																					case MGPIO_OPEN_DRAIN		: SET_BIT(GPIOODR(loc_u8_PortNum)	, loc_u8_PinNum );			break;
-																					case MGPIO_MAMP_2				:	SET_BIT(GPIODR2R(loc_u8_PortNum), loc_u8_PinNum );			break;
-																					case MGPIO_MAMP_4				:	SET_BIT(GPIODR4R(loc_u8_PortNum), loc_u8_PinNum );			break;
-																					case MGPIO_MAMP_8				:	SET_BIT(GPIODR8R(loc_u8_PortNum), loc_u8_PinNum );			break;
-																					default:  break;
+																					SET_BIT(GPIOODR(loc_u8_PortNum)	, loc_u8_PinNum );
 																				}
-																				break;
-								default:break;
+																				else if(st_gpio_cfg->un_gpio_conf.enu_gpio_amp_mode ==MGPIO_MAMP_2)
+																				{
+																					SET_BIT(GPIODR2R(loc_u8_PortNum), loc_u8_PinNum );
+																				}
+																				else if(st_gpio_cfg->un_gpio_conf.enu_gpio_amp_mode == MGPIO_MAMP_4)
+																				{
+																					SET_BIT(GPIODR4R(loc_u8_PortNum), loc_u8_PinNum );
+																				}
+																				else if(st_gpio_cfg->un_gpio_conf.enu_gpio_amp_mode == MGPIO_MAMP_8)
+																				{
+																					SET_BIT(GPIODR8R(loc_u8_PortNum), loc_u8_PinNum );
+																				}
+																				else
+																				{
+																					/* do nothing */
+																				}
+																				
+																				
+								default:;
 							}
+							
 							CLR_BIT(GPIOAFSEL(loc_u8_PortNum), loc_u8_PinNum);
-							GPIODATA(loc_u8_PortNum) &= 0xFFFFFF00;
-							SET_BIT(GPIODEN(loc_u8_PortNum), loc_u8_PinNum );
-							break;
-
-				case MGPIO_MODE_ANALOG: CLR_BIT(GPIODEN(loc_u8_PortNum), loc_u8_PinNum );	 			break;
-				case MGPIO_MODE_ALTF	: SET_BIT(GPIOAFSEL(loc_u8_PortNum), loc_u8_PinNum);			break;
-				default:			break;
-
+							SET_BIT(GPIODEN(loc_u8_PortNum), loc_u8_PinNum );				
+				
 			}
+			else if(st_gpio_cfg->enu_gpio_mode == MGPIO_MODE_ANALOG)
+			{
+				CLR_BIT(GPIODEN(loc_u8_PortNum), loc_u8_PinNum );	
+			}
+			else if(st_gpio_cfg->enu_gpio_mode == MGPIO_MODE_ALTF)
+			{
+				SET_BIT(GPIOAFSEL(loc_u8_PortNum), loc_u8_PinNum);	
+			}
+			else 
+			{
+				/* do nothing */
+			}
+			
 		}
 		else
 		{
@@ -116,13 +148,24 @@ uint8_ MGPIO_u8SetPinData(enu_pin_t Copy_enPinNum, uint8_ Copy_PinValue)
 		loc_u8_PortNum = Copy_enPinNum / 8;
 		loc_u8_PinNum = Copy_enPinNum % 8;
 
-		switch (Copy_PinValue)
+		
+		if(Copy_PinValue == MGPIO_PIN_LOW)
 		{
-			case MGPIO_PIN_LOW	: CLR_BIT(GPIODATA(loc_u8_PortNum), loc_u8_PinNum );	 	break;
-			case MGPIO_PIN_HIGH	: SET_BIT(GPIODATA(loc_u8_PortNum), loc_u8_PinNum );	 	break;
-			case MGPIO_PIN_TOG	: TOG_BIT(GPIODATA(loc_u8_PortNum), loc_u8_PinNum );  	break;
-			default: break;
+			CLR_BIT(GPIODATA(loc_u8_PortNum), loc_u8_PinNum );
 		}
+		else if(Copy_PinValue == MGPIO_PIN_HIGH)
+		{
+			SET_BIT(GPIODATA(loc_u8_PortNum), loc_u8_PinNum );	
+		}
+		else if(Copy_PinValue == MGPIO_PIN_TOG)
+		{
+			TOG_BIT(GPIODATA(loc_u8_PortNum), loc_u8_PinNum ); 
+		}
+		else 
+		{
+			/* do nothing */
+		}
+		
 	}else
 	{
 	loc_u8_error_status  = MGPIO_FAILED;
@@ -162,19 +205,29 @@ uint8_ MGPIO_u8IRQEnable(enu_pin_t Copy_enPinNum, enu_int_sens_type_t enu_int_se
 		loc_u8_PortNum = Copy_enPinNum / 8;
 		loc_u8_PinNum = Copy_enPinNum % 8;	
 		
+		
 			switch(enu_int_sens_type)
 			{
 				case MGPIO_INT_EDGE_SENSETIVE	: CLR_BIT(GPIOIS(loc_u8_PortNum), loc_u8_PinNum );						break;
-				case MGPIO_INT_LEVEL_SENSETIVE: SET_BIT(GPIOIS(loc_u8_PortNum), loc_u8_PinNum );						break;
-				default: break;
+				case MGPIO_INT_LEVEL_SENSETIVE: SET_BIT(GPIOIS(loc_u8_PortNum), loc_u8_PinNum );
+				default: ;
 			}
 			
-			switch (enu_int_sens_ctrl)
+			if (enu_int_sens_ctrl == MGPIO_INT_BOTH_EDGES)
 			{
-				case MGPIO_INT_BOTH_EDGES		: SET_BIT(GPIOIBE(loc_u8_PortNum), loc_u8_PinNum );						break;
-				case MGPIO_INT_FALL_E_LOW_L	: CLR_BIT(GPIOIEV(loc_u8_PortNum), loc_u8_PinNum );						break;
-				case MGPIO_INT_RIS_E_HIGH_L	: SET_BIT(GPIOIEV(loc_u8_PortNum), loc_u8_PinNum );						break;
-				default: break;	
+				SET_BIT(GPIOIBE(loc_u8_PortNum), loc_u8_PinNum );	
+			}
+			else if(enu_int_sens_ctrl == MGPIO_INT_FALL_E_LOW_L)
+			{
+				CLR_BIT(GPIOIEV(loc_u8_PortNum), loc_u8_PinNum );	
+			}
+			else if(enu_int_sens_ctrl == MGPIO_INT_RIS_E_HIGH_L )
+			{
+				SET_BIT(GPIOIEV(loc_u8_PortNum), loc_u8_PinNum );	
+			}
+			else 
+			{
+				/* do nothing */
 			}
 			
 			
